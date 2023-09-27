c0d2b840e74a7f5e12283c478eb774434bfedc03
Dat Duong <duongquangdat172004@gmail.com>
2023-09-27 09:13:37 +0700

    hehe

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,13 +1,3 @@
 
 
-﻿using System;
-namespace Lab1
-{
-    public class Main{
-        public static void main(string[] args)
-        {
-            Console.WriteLine("Hello world");
-            Console.WriteLine("Project branch");
-        }
-    }
-}
+Console.WriteLine("Hello world");
