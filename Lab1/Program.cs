e9d97871f924c8fcc8fa6e042d55cd7c53ac1675
Dat Duong <duongquangdat172004@gmail.com>
2023-09-27 09:11:37 +0700

    test

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -7,7 +7,7 @@ namespace Lab1
         public static void main(string[] args)
         {
             Console.WriteLine("Hello world");
-    
+            Console.WriteLine("Project branch");
         }
     }
 }
